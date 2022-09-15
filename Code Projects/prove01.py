ac24ffcc3f8f9819c133ec463e76f317cab0306b
ZorroSavage117 <zorrosavage117@byui.edu>
2022-09-15 11:52:41 -0600

    code added

--- Code Projects/prove01.py
+++ Code Projects/prove01.py
@@ -5,6 +5,7 @@ print()
 color = "unknown"
 name = "unknown"
 city = "unknown"
+miles = 0
 
 # main program
 print("Hello there, I am a travaler seeking a place to live. \nMy name is Zorro the Forgotten.")
@@ -13,7 +14,10 @@ print(f"Well its nice to meet you, {name}.")
 print("I seem to have a need to ask you what your favorite color is, but first mine is blood red.")
 color = input("And what color do you like? \n")
 print("That is a lovely color you have chosen.")
-city = input("One last question, what city or place do you recommend to live at? \n")
+city = input("Two more questions, what city or place do you recommend to live at? \n")
+miles = input(f"How far is {city} from here in miles? (Enter a number please)\n")
+miles = int(miles)
+print(f"Thats just over younder an\'t it, at exactly {miles} miles.")
 print(f"Thank you, {name}. I think I am going to start heading to {city}. \nI think I will paint a wall or two in my place the color of {color}.")
 
 # page spacing
