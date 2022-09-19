2c0a52e2499d0f337c53432d638d0335b07077da
ZorroSavage117 <zorrosavage117@byui.edu>
2022-09-19 12:17:54 -0600

    prep checkpoint 2

---
+++ Code Projects/bondintro.py
@@ -0,0 +1,13 @@
+# Variables
+from distutils.command.build_scripts import first_line_re
+
+# Variables
+first_name = "unknown"
+last_name = "unknown"
+
+# Main code
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+print(f"\nYour name is {last_name}, {first_name} {last_name}.")
+
+# Last line
