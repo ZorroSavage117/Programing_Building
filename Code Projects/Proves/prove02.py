31c7ba35520adc7e5f08ce6fdef8a525390fb87c
ZorroSavage117 <zorrosavage117@byui.edu>
2022-09-23 11:36:24 -0600

    prove 2 start

---
+++ Code Projects/Proves/prove02.py
@@ -0,0 +1,28 @@
+# Page set up
+print()
+
+# Variables
+adjective1 = "unknown"
+animal1 = "unknown"
+verb1 = "unknown"
+exclamation1 = "unknown"
+verb2 = "unknown"
+verb3 = "unknown"
+
+# Main code
+print("Please enter the following:\n")
+adjective1 = input("adjective: ")
+animal1 = input("animal: ")
+verb1 = input("verb: ")
+exclamation1 = input("exclam1ation: ")
+verb2 = input("verb: ")
+verb3 = input("verb: ")
+
+print("\nYour story is:\n")
+print(f"""The other day, I was really in trouble. It all started when I saw a very 
+{adjective1} {animal1} {verb1} down the hallway. \"{exclamation1.capitalize()}\" I yelled. But all 
+I could think to do was to {verb2} over and over. Miraculously, that caused it to stop, but not before it tried to {verb3}
+right in front of my family.""")
+
+# Page set up
+print()
