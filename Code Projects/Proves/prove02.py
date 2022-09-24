69dc7fa89a3e2f254a5c5bf5f80212280817d3db
ZorroSavage117 <zorrosavage117@byui.edu>
2022-09-23 19:13:31 -0600

    exteded madlib

--- Code Projects/Proves/prove02.py
+++ Code Projects/Proves/prove02.py
@@ -8,6 +8,11 @@ verb1 = "unknown"
 exclamation1 = "unknown"
 verb2 = "unknown"
 verb3 = "unknown"
+exclamation2 = "unknown"
+verb4 = "unknown"
+name1 = "unknown"
+noun1 = "unknown"
+verb5 = "unknown"
 
 # Main code
 print("Please enter the following:\n")
@@ -17,12 +22,18 @@ verb1 = input("verb: ")
 exclamation1 = input("exclam1ation: ")
 verb2 = input("verb: ")
 verb3 = input("verb: ")
+exclamation2 = input("exclamation: ")
+verb4 = input("verb: ")
+name1 = input("name: ")
+noun1 = input("noun: ")
+verb5 = input("verb: ")
 
 print("\nYour story is:\n")
 print(f"""The other day, I was really in trouble. It all started when I saw a very 
 {adjective1} {animal1} {verb1} down the hallway. \"{exclamation1.capitalize()}\" I yelled. But all 
 I could think to do was to {verb2} over and over. Miraculously, that caused it to stop, but not before it tried to {verb3}
-right in front of my family.""")
+right in front of my family. Then my sister yelled, \"{exclamation2.upper()}!\" As my sister said this, mother {verb4} me.
+Uncle {name1} grabed the {noun1} and {verb5} out the door.""")
 
 # Page set up
 print()
