4e31cb99d8c1ed775e0108fdc262b72c99794714
ZorroSavage117 <zorrosavage117@byui.edu>
2022-09-23 22:15:05 -0600

    prove 2 update

--- Code Projects/Proves/prove02.py
+++ Code Projects/Proves/prove02.py
@@ -13,6 +13,7 @@ verb4 = "unknown"
 name1 = "unknown"
 noun1 = "unknown"
 verb5 = "unknown"
+story = "unknown"
 
 # Main code
 print("Please enter the following:\n")
@@ -27,13 +28,22 @@ verb4 = input("verb: ")
 name1 = input("name: ")
 noun1 = input("noun: ")
 verb5 = input("verb: ")
+story = input("a or b: ")
 
-print("\nYour story is:\n")
-print(f"""The other day, I was really in trouble. It all started when I saw a very 
+print(f"\nYour story is: {story}\n")
+
+if story.lower() == "a":
+    print(f"""The other day, I was really in trouble. It all started when I saw a very 
 {adjective1} {animal1} {verb1} down the hallway. \"{exclamation1.capitalize()}\" I yelled. But all 
 I could think to do was to {verb2} over and over. Miraculously, that caused it to stop, but not before it tried to {verb3}
 right in front of my family. Then my sister yelled, \"{exclamation2.upper()}!\" As my sister said this, mother {verb4} me.
 Uncle {name1} grabed the {noun1} and {verb5} out the door.""")
+elif story.lower() == "b":
+    print(f"""The {adjective1} man ran in to a {animal1} on his {verb1} to the store. \"{exclamation1.capitalize()}\", he exclamed.
+He began to {verb2} at the sight, to no {verb3} nothing happened. He than said, \"{exclamation2.capitalize()}.\" So he {verb4}
+on and went to {name1}\'s Market. He when to buy a {noun1}, it was important so he could {verb5}.""")
+else:
+    print(f"Invalid story ID -- {story} is not one of the options")
 
 # Page set up
 print()
