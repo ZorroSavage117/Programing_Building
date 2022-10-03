b96902a5a6f844cd2bd4cb71abdbd9239b9cd303
ZorroSavage117 <zorrosavage117@byui.edu>
2022-10-03 13:02:40 -0600

    prep check 4

---
+++ Code Projects/Prep Checks/unit_conversion.py
@@ -0,0 +1,19 @@
+# page set up
+import math
+print()
+
+# Varibals
+temp = 0.0
+conver = float(5/9)
+
+# main code
+temp = float(input("What is the temperature in Fahrenheit? "))
+
+temp = temp - 32
+temp = temp * conver
+
+
+print(f"""The temperature in Celsius is {"{:.1f}".format(temp)} degrees.""")
+
+# page set up
+print()
