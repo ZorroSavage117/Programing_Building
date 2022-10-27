52979902e0f5e4cce070df2b850974a15bc24b59
ZorroSavage117 <zorrosavage117@byui.edu>
2022-10-27 14:49:24 -0600

    prove 7&8

--- Testing/test.py
+++ Testing/test.py
@@ -1,1 +1,25 @@
-print ('this is a test')
+# start page
+print()
+print('this is a test')
+print()
+
+# retuns the leter at postion 'x'
+# x = 0
+# a = "Hello, World!"
+# print(a[x])
+
+# prints out each letter on its own line
+# for x in "banana":
+#     print(x)
+
+# returs a true if this 'string' is in the origanal string
+# txt = "The best things in life are free!"
+# print("free" in txt)
+
+# returns a true if this 'string' is not in the origanal string
+# txt = "The best things in life are free!"
+# print("expensive" not in txt)
+
+# searches the string for a specified value and
+# returns the position of where it was found
+# index() or find()
